77d971de1a7442f21dc5d0baa696961b739c3bf2
Treyvin Daniels <Kingtrey32@gmail.com>
2022-07-10 14:46:15 -0400

    made a change in eclispe

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,7 +3,7 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Dolly");
+		System.out.println("Hello Dolly from eclispe again");
 
 	}
 
